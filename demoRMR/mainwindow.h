b6d99e55b17d9c37618fbc610316b4785863ec88
Adrián <xfalba@stuba.sk>
2022-09-04 17:38:27 +0200

    Make mainwindow code more readable

--- demoRMR/mainwindow.h
+++ demoRMR/mainwindow.h
@@ -25,17 +25,17 @@
 #include "robot.h"
 
 #include <QJoysticks.h>
+
 namespace Ui {
-class MainWindow;
+    class MainWindow;
 }
 
-class MainWindow : public QMainWindow
-{
+class MainWindow : public QMainWindow {
     Q_OBJECT
 
 public:
     bool useCamera1;
-  //  cv::VideoCapture cap;
+    //  cv::VideoCapture cap;
 
     int actIndex;
     //    cv::Mat frame[3];
@@ -45,48 +45,42 @@ public:
     ~MainWindow();
 
     int processThisLidar(LaserMeasurement laserData);
-
     int processThisRobot(TKobukiData robotdata);
-
-int processThisCamera(cv::Mat cameraData);
+    int processThisCamera(cv::Mat cameraData);
 
 private slots:
     void on_pushButton_9_clicked();
-
     void on_pushButton_2_clicked();
-
     void on_pushButton_3_clicked();
-
     void on_pushButton_6_clicked();
-
     void on_pushButton_5_clicked();
-
     void on_pushButton_4_clicked();
-
     void on_pushButton_clicked();
     void getNewFrame();
 
 private:
-     JOYINFO joystickInfo;
+
+    JOYINFO joystickInfo;
     Ui::MainWindow *ui;
-     void paintEvent(QPaintEvent *event);// Q_DECL_OVERRIDE;
-     int updateLaserPicture;
-     LaserMeasurement copyOfLaserData;
-     std::string ipaddress;
-     Robot robot;
-     TKobukiData robotdata;
-     int datacounter;
-     QTimer *timer;
+    void paintEvent(QPaintEvent *event);// Q_DECL_OVERRIDE;
+    int updateLaserPicture;
+    LaserMeasurement copyOfLaserData;
+    std::string ipaddress;
+    Robot robot;
+    TKobukiData robotdata;
+    int datacounter;
+    QTimer *timer;
 
-     QJoysticks *instance;
+    QJoysticks *instance;
 
-     double forwardspeed;//mm/s
-     double rotationspeed;//omega/s
+    double forwardspeed;//mm/s
+    double rotationspeed;//omega/s
+
 public slots:
-     void setUiValues(double robotX,double robotY,double robotFi);
-signals:
-     void uiValuesChanged(double newrobotX,double newrobotY,double newrobotFi); ///toto nema telo
+    void setUiValues(double robotX,double robotY,double robotFi);
 
+signals:
+    void uiValuesChanged(double newrobotX,double newrobotY,double newrobotFi); ///toto nema telo
 
 };
 
